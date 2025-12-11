187c7c883ec8c2a301da52aa2d18a9faa27813c5
OfficialMrDipak <133913371+OfficialMrDipak@users.noreply.github.com>
2025-12-11 11:42:56 +0530

    Create outputs.tf

---
+++ modules/virtual-network/modules/virtual-network/modules/virtual-network/outputs.tf
@@ -0,0 +1,21 @@
+output "vnet_id" {
+  description = "ID of the virtual network"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_name" {
+  description = "Name of the virtual network"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_ids" {
+  description = "Map of subnet names to IDs"
+  value = {
+    for k, v in azurerm_subnet.subnets : k => v.id
+  }
+}
+
+output "address_space" {
+  description = "Address space of the VNet"
+  value       = azurerm_virtual_network.vnet.address_space
+}
