ca496c4779b692a837ddd1663bd6162bd7c23908
OfficialMrDipak <133913371+OfficialMrDipak@users.noreply.github.com>
2025-12-11 11:40:45 +0530

    Create variables.tf

---
+++ modules/virtual-network/modules/virtual-network/variables.tf
@@ -0,0 +1,41 @@
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for VNet"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnets" {
+  description = "Map of subnet names to address prefixes"
+  type        = map(string)
+  default = {
+    default = "10.0.1.0/24"
+  }
+}
+
+variable "dns_servers" {
+  description = "List of DNS server IPs"
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
